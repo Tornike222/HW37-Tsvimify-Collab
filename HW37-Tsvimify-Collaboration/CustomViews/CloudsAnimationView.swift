3499540b44bc639febd8ff96ed5d2924a6a10e6f
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-06-12 18:25:40 +0400

    Add custom component for clouds animation

--- HW37-Tsvimify-Collaboration/CustomViews/CloudsAnimationView.swift
+++ HW37-Tsvimify-Collaboration/CustomViews/CloudsAnimationView.swift
@@ -5,4 +5,17 @@
 //  Created by Irinka Datoshvili on 12.06.24.
 //
 
-import Foundation
+import SpriteKit
+import SwiftUI
+
+struct CloudAnimationView: View {
+    @Environment(\.colorScheme) var colorScheme
+    let sksFileName: String
+    
+    var body: some View {
+        Group {
+            SpriteView(scene: CloudsAnimation(anchorPointX: -1, anchorPointY: 0, sksFileName: sksFileName, particleColor: colorScheme == .light ? .white : .gray), options: [.allowsTransparency])
+                .ignoresSafeArea()
+        }
+    }
+}
