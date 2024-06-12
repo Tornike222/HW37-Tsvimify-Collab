d1d81dd756121a3579a02629a36e8176ad9c95fb
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-06-12 17:47:14 +0400

    Add stars twinkling animation for warm weather background

---
+++ HW37-Tsvimify-Collaboration/Backgrounds/CloudyBackground.swift
@@ -0,0 +1,8 @@
+//
+//  CloudyBackground.swift
+//  HW37-Tsvimify-Collaboration
+//
+//  Created by Irinka Datoshvili on 12.06.24.
+//
+
+import Foundation
---
+++ HW37-Tsvimify-Collaboration/Backgrounds/WarmBackground.swift
@@ -0,0 +1,8 @@
+//
+//  WarmBackground.swift
+//  HW37-Tsvimify-Collaboration
+//
+//  Created by Irinka Datoshvili on 12.06.24.
+//
+
+import Foundation
