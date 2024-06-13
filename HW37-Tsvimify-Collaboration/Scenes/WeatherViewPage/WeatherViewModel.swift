0644fec5ffcc22a32cc0be294fe6d060772aa61b
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-06-13 10:50:46 +0400

    Add methods for fetch and processing weekly weather forcasts

--- HW37-Tsvimify-Collaboration/Scenes/WeatherViewPage/WeatherViewModel.swift
+++ HW37-Tsvimify-Collaboration/Scenes/WeatherViewPage/WeatherViewModel.swift
@@ -31,6 +31,40 @@ class WeatherViewModel: ObservableObject {
             }
         }
     }
+    
+    func fetchWeekForecast(lat: Double, lon: Double) {
+        let urlString = "https://openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02"
+        
+        NetworkService().requestData(urlString: urlString) { (response: WeekForecastResponse?, error: Error?) in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+                return
+            }
+            
+            if let response = response {
+                self.weekForecastResponse = response
+            } else {
+                self.errorMessage = "Failed to load forecast data."
+            }
+        }
+    }
+    
+    func dayOfWeek(from timestamp: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: date)
+    }
+    
+    func sortedWeekData() -> [DailyWeather] {
+        guard let data = weekForecastResponse?.daily else { return [] }
+        let sortedData = data.sorted(by: { (first, second) -> Bool in
+            let weekdayFirst = (Calendar.current.component(.weekday, from: Date(timeIntervalSince1970: TimeInterval(first.dt))) + 5) % 7
+            let weekdaySecond = (Calendar.current.component(.weekday, from: Date(timeIntervalSince1970: TimeInterval(second.dt))) + 5) % 7
+            return weekdayFirst < weekdaySecond
+        })
+        return sortedData
+    }
 }
 
 
